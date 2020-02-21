c1fd69b3528b2e5449158ade126e7b0adc982f25
Flo Leinenkugel <flolein@gmx.net>
2020-02-21 15:27:26 +0100

    finished the scaffold with mecanics, still ugly

--- Gemfile
+++ Gemfile
@@ -38,7 +38,6 @@ gem 'autoprefixer-rails'
 gem 'font-awesome-sass', '~> 5.6.1'
 gem 'simple_form'
 
-
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "Lime juice")
+Ingredient.create(name: "Creme de Cacao")
+Ingredient.create(name: "Spiced rum")
+Ingredient.create(name: "Grenadine")
+Ingredient.create(name: "Peppermint")
+Ingredient.create(name: "Irish Cream")
+Ingredient.create(name: "Apple Juice")
+Ingredient.create(name: "Coca Cola")
+Ingredient.create(name: "Orange juice")
+
+Cocktail.create(
+  name: "sicker pinch"
+  #image_url: 'https://images.unsplash.com/photo-1545438102-799c3991ffb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+  )
+Cocktail.create(
+  name: "socker ponch"
+  #image_url: 'https://images.unsplash.com/photo-1545438102-799c3991ffb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+  )
+Cocktail.create(
+  name: "secker pench"
+ # image_url: 'https://images.unsplash.com/photo-1545438102-799c3991ffb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+  )
+Cocktail.create(
+  name: "sacker panch"
+  #image_url: 'https://images.unsplash.com/photo-1545438102-799c3991ffb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+  )
+Cocktail.create(
+  name: "sucker punch"
+  #image_url: 'https://images.unsplash.com/photo-1545438102-799c3991ffb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+  )
+
+
+
+
+
+# Cocktail.create(name: "secker pench")
+# Cocktail.create(name: "sacker panch")
+# Cocktail.create(name: "sucker punch")
+
+
+
+
